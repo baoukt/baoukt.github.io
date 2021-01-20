cb1f6deb435bef00aaa299f8f70aeacf63eddbb7
Alex Li <baoukt@Alexs-MacBook-Pro.local>
2021-01-19 20:05:40 -0500

    Committing changes with external JSON file

--- scripts/index.js
+++ scripts/index.js
@@ -15,60 +15,61 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
 
-$( document ).ready(function() {
-  $( ".x-image" ).click(function() {
-    $( ".urgent-banner" ).hide();
+function filterEvents(item, index) {
+  console.log(index);
+
+  let eventDate = Date.parse(item.date);
+  let today = Date.now();
+
+  if (eventDate > today && index < 3) {
+    document.getElementById("events").innerHTML += "<div class='tile tile-"
+        + (index + 1) + "'><text class='event-text'>" + item.name + "</text></div>";
+  }
+
+}
+
+$(document).ready(function () {
+  $(".x-image").click(function () {
+    $(".urgent-banner").hide();
   });
 
-  $("button.events-button").click(function() {
+  $("button.events-button").click(function () {
     $('html,body').animate({
-      scrollTop: $(".events-container").offset().top},
-      'slow');
+          scrollTop: $(".events-container").offset().top
+        },
+        'slow');
   });
 
-  $("button.aboutus").click(function() {
+  $("button.aboutus").click(function () {
     $('html,body').animate({
-      scrollTop: $(".about-container").offset().top},
-      'slow');
+          scrollTop: $(".about-container").offset().top
+        },
+        'slow');
+  });
+
+  // Reading in values from the external JSON file with the D3 library
+  d3.json("events.json", function (data) {
+    let eventLength = data.events.length;
+    data.events.forEach(filterEvents);
+
+    console.log(data);
+    console.log(eventLength);
   });
 
 });
-
-//events
-var count = 0;
-var json = {
-	"events":[
-   {"date":'20 Jan 2020 00:00:00 EST', "name":"mlk day", "desc":"happy mlk day 2020"},
-   {"date":'20 Jan 2021 00:00:00 EST', "name":"mlk day 2021", "desc":"happy mlk day"},
-    {"date":'14 Feb 2021 00:00:00 EST', "name":"valentines", "desc":"happy valentines"},
-    {"date": '24 Apr 2021 00:00:00 EST', "name":"nyc", "desc":"spring conference 2021"}
-  ]
-}
-
-function filter (item, index) { 
-	var countid = "event"+(count+1);
-	var eventdate = Date.parse(item.date);
-  var today = Date.now();
-  if(eventdate > today && count<3){
-  	document.getElementById(countid).innerHTML += 
-      '<text class="event-title">' + item.name + '</text>' +
-      '<text class="event-text">' + item.desc + '</text>'+
-      '<text class="event-text">' + eventdate.getDay() + '</text>';
-      //wait it says .getDay() is not a function?? same for getMonth and getYear.
-    count++; 
-  }
-}
-
-json.events.forEach(filter);
