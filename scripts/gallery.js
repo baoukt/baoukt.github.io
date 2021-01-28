66e947fbce87c4411b7d35fca43816dd1462cfc4
BotDestroyer <59812447+BotDestroyer@users.noreply.github.com>
2021-01-28 17:43:03 -0500

    finished gallery

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,11 +1,83 @@
 $(document).ready(function () {
   $("img.event-1-img").on("click", function (event) {
-    $("div.pa-gallery-player-widget").show();
-    $("img.close").show();
+    $("div.embed-container").append("<script src=\"https://cdn.jsdelivr.net/npm/publicalbum@latest/embed-ui.min.js\" async></script>\n" +
+        "<div class=\"pa-gallery-player-widget\" style=display:none;\"\n" +
+        "  data-link=\"https://photos.app.goo.gl/62rypZ96ZFnWr6H16\"\n" +
+        "  data-title=\"Weekend in Kidder Township\"\n" +
+        "  data-description=\"29 new photos · Album by Paul Wang\">\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/tRCYpAvlNJOoBjq_iV8Ot-ObhGUD4b3jJvQrh2NdGxcEe_7DYYytjuec80iww6R-2-bXydsbNCNFb60jVsVfJ0T_91sUAHtgCzqlnu-XL9w1TAh0dwswDg4C-AotYJYofOe8dsLo_KI=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/EmGlFA3WbpHCVkAKJ2vBntiKDH9VWMuajaxwYufwCl3KLykBQshfRm0KOlacrglDqzwh3yRONif0Dy77rv0xd8QnZzVJ8ARGklck6Sx7AyLSv0OEIVS4jkwid6KEK0h0VjdtelUXR2w=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/rsHV1gvX6NVbgwr2POEUm0pfoD8UoTnrau0I3A2cELUY1vS-6VyGSr5wWRN_3U0_2yqSIEpORNCW5Pb-25M4e8MbiTaECHatR27Ff5IuXFVn7uwdALW7RmVflaTU9QrLfrtJfxtgaa0=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/f9aEtStrvq5uWosIprrdDRsOz0i9bwRmRE2OLKR3BW4PLUtutVlhnee86ElbEAo8q8bo_m5WWSnrTTMXI_3pQ8MT-8t8OcUEKQjEjvndquN9PvN-eXFa9WBGUt4lRyrsXNNR4kxqbLI=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/db5YCzp-F8SpNPuy9INykWlscYE9uGAOVjIbg7iSGldJwhzL1qr_BiY-LuRbw8r-_fXoBqUXaQpIohYLTLW9KRSAuUg6XaVPQYCwA7REbwkbmOtIe3Gj-s5q1xiiiG738uY5LwU9KsU=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/t10pPfu9XY_qThSdwGgW_muqGW93cwYjV11eSPo2eVgRpXDVo37QoMOt0sElmW_qHQklEPXRiZ1_OFDZm-IPoXH6q6fGyrm2jBqGKLUNlBZR-ej80ywj1wP7XoCFeyBrz7o8UubhMLc=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/eeS8ZYYBXCHal-Ak-30gmNI8NnLWy8dNWHio3G1CshRXKs8j8AFSuC5ax4XibeCqAYaSfRrXYovNV1r_OLSWJFI8UZXbViaY-ySAXd6BWL-_Xpo2S9M6wPII902KkBOzZiwom7FLrTU=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/44gzeh6vjwFvEXeiuvrOYnkXUQS67SL-mr019va540Z5-DJE9pzjvYe0ltJlZsCtcBw07lWuE-qjt-MdtkEULkybnhKvjTJLyfbtiHl_8M2t1pqsIjtr8r-3bE2UYHbTNQ3k851WI34=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/kZ0SY2ygyjIhjQ73mVvZpQK9DBKhYzLrxZTpznUF_kvYKmOXDucKe6ICJv0Ko3qCceNxWnvem0IlFlfQ2NDlp3ZadLIq9Ujk5-5Eyj8ThkDI4A4mSuASddo-S9kXLCzmLkVzDh_5V6A=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/wDtAN37p0_t-bLC8gVFUxkbm3ky-R5iIYKhc9HdT7RRkP9JqNGFLdK7V0DjCaFVt4YvDh7unK-LKiIV_SKpVASpNxwXZ0G1mAALwvC3xUJKJiNsFx_8bhYKBv61Cbx77SJjqMnt7dO8=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/HGKJbImdkpIPvTzW6a5zdJjsofCdPYOi7RMtMDgoFuMzmovZ9dDme76ud0RAz_MCQ5RyhOFy7R9LIqtztwRnh6G8BGylyu2LstXxHITSFizL-mH-SPcv90sbuCkHF9mw35wY2Y4z_vE=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/xIuO7K0D2wUBcsZGpv479DCl3ukYbAf-tMFAl5le83T0spZcux-b6ipOOytbbyw_5XFx5G0VZRywARo__PoDqubigJMwXJmS_7DcA5Fisr2aTeVf0xdr8fIHiNi50l6SRq5sWRDlKPg=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/qUVujVVguZGwPTO5vSRxiaF2EPME_VUomdsCWVWAsEQkBt876aBKxyDs0ysV17H-lPmL3qoztwP_d0Lk5h4XbmHZrdv_IqCwln0jm-nQWOqlNMNLzwh7yIeFtEI3USm72NYYrjEZLZ8=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/ELKvTM6kcjdZJqhlCZWO85vwtznsBRsdO2LdxOCrQHkgKmT-bIkTIyb9NZTCAauHcV-ium5mRLXXt4SxPgkOoTClyZwLeUJ2NxSQwSIXrN1UGMpBzdvI2r4dtMotAUpoa_lkP3we0Es=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/p_ew6kxmp0zW6ICQijGkEZzGM3I0szT1yAIR4UCczVPZ5fDI_8iHTBFosdAsQknvUGenfuc-6rEIAgD3vTRNlYlZM4KSMX2FFsaChftgmYsQ5rZGZIxe1R92XBVIv7s19jaj0Taa1oo=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/8U-g3DKwAMqbuH5_WY65edXBGwEihf654Uofg6nodROPTd98vm5fq8rrpCj4nLOEPb-UEMZq94yq1gbEWGd9l0ZW9HiGxgcsU2eqQMLFje_Ty5qP8xAwShT8UfJFCapLHtvrVgOquqs=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/Aa-OQfnLQcX9oK_g60jKXqapNqYvhl4cBuMQ8B3lWxmiRNfa-Ji-_c2OUGlajthAud2KLxfGDOBb_Yn4_eiOi3iVIjPKl65DHSGo7FTk1dWjO84T1IMc8e8WV-wRPTslKw3mhYwQbE0=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/S2npnPuZwopsLmUKmcvcO9aR8SukYcDXRq2Qx4UqV_ppwMTRRjLo_-eAU_LpFEAmIsfTCI4w5WMffo1AaJKjLudedtj86znJQNqldXaQ6CFcf6LyPxk62qIyrB2hfwbegk7otMj3OrM=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/LivRtPdmsfxvtpDhzytS1LdHiOcTrBWDxV5HUAYOyUwJO9t93QZrisxOGANKQ2eBEWMUpS0a9kZap_tGuBXXZrhWjEPTFjtxiIWkpLn6AYMofwaq3nY5aUkxnQEiioBEWIVes_M3TDQ=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/VD--8tMFxp9rc4o2LnEoSJKifbKdJ2wX9v0yDhD08U1xWw82vf7DTApynFTvl9U-jXXM2xv7B-P26ori5GLEQnQN-zkqLLHacssvpIdAgXM5ch7gQh3rEfcxpOIpTO5bheOEHgQdEkY=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/P6syg7EOkL-3rl83ROPLO1tLMNIALnSwbDsu14n4ZUlr3otuDvUrEkBC8_s2C8W9z2-kNNmfBCK5e0GXs-cm8cZZfflZraURbyObr3p7HmG3GgAq0FyPH6Dsdf8IOZb3aVhBfUkMfP4=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/exMYm4UM1sbZOIwsfKuyPt4uErQACtfNC875vlg8Q0fUPYLSWJ0uLU3lT8yie0DUMQbcq82HoGYWxsUkh5wnjpb9YXUHZHEz6jO7jqRD1air_HNEE0ghyUApSIj1PHFkPwy-dm2vY9M=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/pwm5yTNaEWOroy5XRFeT0sysYah3lXytv1o7ujDzmMrIFFHJsgiWWP5w9frjr8MkNdo-H-GBGrQfymWtEj9MsrUPbkJXCYSTdfrEVMAwYjT5S0VMbQQNBytwTBRrV7ZJ_AjQFUBotPI=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/zzef0kxU5UznaN5xuv9SMaSpAT7l-mZDTmeb7X3o_RvCAdt1Qw3zlDzRWs00Cdp3R2dFp9U44tjanFtFEWAm-z5S41OucjSlyD9Gd-SAGutEpt5WS8w5vjfS57xcibJzBsiMQF8y_js=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/9mYDklDKMB4OaAU-bJIyodkokto3oaJcY4GzsW4Ka5yF39THTZlf1Fj0uG_3VTWaoi6kElZBI3CDkACKftni4WhTlWS4YJaQ06wJlFf5u5VEXNntk2nXCuNhwRbF8oLdE15DHa6QOis=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/hiGMsEyum7WP0m7_Ne48zLorr8VnnPiytNhyuxRRwTWtShsRgmhw_Zlx-iQH-_RzG_dsGfU209v1nm0Gk0iy0ec45BcVd9pRB8hqhkJCZBKH7nUXIc6mNgbp-jp1mqBn5UfWE74EubA=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/rbWCdKYQpEiR7Na_aax0wL2V1AxxDSQuTE6YfBh1anAI0FZJhWp5qyTOKj_FjSnAmOg9KKWyGWKcHI_Dm-8qYHVU38ELX7jY1dNLdn69CdxLINfdvGu-eYKooNl_GRlc0obAA0nd234=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/moOmb3RDqZekOzYiWw5TbijpSYX3e69gYRvhDELOWCy4e_euq1UxGT35QNXgC16YoOjkLnZdCryOm0LAYPliKg2q8e2y0SsC7HFRMW8dx_WR4It4LarCDMQb1TdlQf5tEfx6W1VaJbk=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/CnHGrtQqa02ZW1R10f-44RTGLVJnZRil6cO-HazuIdyODty9b4Noxp1h6ULwjF7wBzorC4kohOQQlh0p99G1z_QOtAm544NIAENXKNiWY2r7P0ERfGWmCXeEA2omRvTAmqSftviOFII=w1920-h1080\"></object>\n" +
+        "</div>\n");
+    $("div.overlay-wrapper").show();
+  });
+
+  $("img.event-2-img").on("click", function (event) {
+    $("div.embed-container").append("<script src=\"https://cdn.jsdelivr.net/npm/publicalbum@latest/embed-ui.min.js\" async></script>\n" +
+        "<div class=\"pa-gallery-player-widget\" style= display:none;\"\n" +
+        "  data-link=\"https://photos.app.goo.gl/rFxWtDqXj4CySZ926\"\n" +
+        "  data-title=\"How small Thou are (Hymn 1110)\"\n" +
+        "  data-description=\"2 new photos · Album by Paul Wang\">\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/cO36rjasROdgMMmEXoUG9fdqA7YebKp5zLL4ZE4UzbBUTUNDYIg3gfcRk6lo9kBihu6HOiG2yL3z3b2XlvlZDo8XxAbMvRAKMF054UboCXCA4L5cKn3fpZvrA1pErVXCbHI7eKHcF9c=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/moYQYTftdJIj9s5RKzkXgKO530fxuDUn-0jwgCCvVHX4m2w35EinFX_yRH1XmvyEEpSoyK1-CsGZGVgC1zO0FntSLaIMG4YZUfX-pHCK-Y-e7sBON7ieLXcDfnjHNNy1Zyr7AMQXJG8=w1920-h1080\"></object>\n" +
+        "</div>\n");
+    $("div.overlay-wrapper").show();
+  });
+
+  $("img.event-3-img").on("click", function (event) {
+    $("div.embed-container").append("<script src=\"https://cdn.jsdelivr.net/npm/publicalbum@latest/embed-ui.min.js\" async></script>\n" +
+        "<div class=\"pa-gallery-player-widget\" style=display:none;\"\n" +
+        "  data-link=\"https://goo.gl/photos/udZNXbVeoxCiHQwW7\"\n" +
+        "  data-title=\"Weekend in McGaheysville\"\n" +
+        "  data-description=\"16 new photos\">\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/fzeXngUP58YLJI5UwsGJcoUrSeh1XKWl3lr28Rf-yd_025X153AMh5RjQK2qYmJb1GibQn9DldePpmFshDt72-T2nWB7r9jGTkwhGSDvufRH4XGV6BdTP8u2D9Ku_HP_iL-JSa0z2UA=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/bdTa4PR8q1rEqk_1_ngK5lRHZdliG3qIE5IbShtDef6bCEHhwlV1D4sTsT_XWx6YqfVpWBHtWroGFwX5kDA2qgoakc2_fOzycSVW11WwPXxychyXVUXEFXllkP0AzVV4JsWBV_MxVhY=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/Ha_sVydlf49mVhDPp9YaG1OKuJOFUuMQtOywX9Q5_9dZCDY5MbrsXdwknHPpMCQRtse03UIkZq0rlt9FKTHGmpZt26T2dW6Va4seR_7JEkkpzDYRJG18ry5mmXs-WYO5CcNBsteDKTI=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/_1VGqr__me6TJvVSeGCj3wuwWTW-wiaB5lpBNdx8i00U5JMkRis9DXBcfyFLDcPrqFmBXhZs-jvKk3UXZGKYSNOGsi7tKM8EQkXQYQtwtDvhgYQRgn0Pi0883fmEqaYtJubFnlxHdQI=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/9Zcx6VKy5X0pPoNtPQCWlrGhWH6HbhDR4yRltQF9I5cwN-PQZ_tHI0Iv9ntle2Ehfs4sxz27-tYkSBLnBMwdwFHsE4VL9VK1UlKdd0Xa3CPzUrSibFy4Rk76ZQK8h4KSN8yGUTjRlZA=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/9bR836mrAQnCEeR_JH-D-83vQrYuE31c70WTI6WTSHxN_xVkfEREqoq18oh8DKStghqjPgbQf_XJuPWGDk-YnVwy0NyY3tjKsaYmOGClpT0e9bLro1IFL5Gt4in9pKe6PoJ0317PRZU=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/Vp0hJ1zw-FD4tGL9k_JsaZXF2lWixK_6RvDHA_OEH6WQa4T-UjY3eWapWtaP3XFJNNHYOCk7-O9MRGevlnKJR3WO_Y5Vj-x__ACPGh5szf48-TDHtWyYbx4fjviDWCtuaK1KibOnSTg=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/QAFSoRwceegf9AaEmFPHjsrGShevsr4tdB2wE_vhne9oEGzF05kvvuwMOqg7byNfUo166OOU3nls32XGlg4pjcb3hBjmSFVpycgUS3rXXzj5Qzzt4YIUxfxtqUQtYBWHKmmfRN1N3Jg=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/VFAq77ivII_uG7whBhLO264riicUzisU05hWXbx8wwVVSQ_hr3EXCdNUrX0tr80XaLw7kCdISJk-vDC2CkSr1QSnvjAWg1DdKiBNdlOnm60ivK7I9PZVIFlZKCOdSBwdNgoGDnhD8g=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/UUemmMPlaRMijR9n5yjDa4m9Bboujb0Xm9Xt7KPKVzZTsEm1dDaLjUbLANlNr_mrVk3QERXnYNf-zkhMMpWWhca9f3Xnxv4oA3kkthXLNM83bfERjfrGKBE0dtJve2FzVGKCYIf0lb8=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/MgMj8mnpeKmnEsi3j9n9n5YbOjl5e7_FyMvD_Sz6NMwP3QLc656GZDCYAR640xNqQoooZSiLoaUMjwVQLVv2pktjUPCKG-dPJksNQNWL56w-KZfVH5tHvNTK-A8J4YLSz-wSG-0dKcU=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/FSqsOJ9JA6t8OZnfyHRoufkxDTwitTZXrx2ZqhghydTXhM6EMkavK-40W2khlzhmVwWRvuTqMj74XLuVD-dzEQZhI3I3DEiOhfQR_LwfTjLu2-3YWHod7sfPLeMCLT4Dna0OsFMC6G8=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/sffAW3_Ni2PBgllkJMa1N4m1Cz7RC-BXC6QSyKOWlUmyOs0zwIdmvN4BegXJmeGKVzOiWkh9Pa-JHEl2yTP207PuZUn5zUPh6cmyOBZFmrcC4WjQWstoMixQ2GQ36tV0MPSzNVuDAvA=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/5sIKCWbc3DR2xszkWwbXj-ppv_BDL3zeL4J1JW4aTnrVP_5ZOzdxgYiVVwsTBY08syWNBnWQMO0zwYE9wSA31nQRefwMXA7lwGRPDD-4JPcA9yjbU_rZks-3zQ8FP_kCOzNlewCO3xw=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/13gZArbz3orh4JgX5-kxoKnEFdpFNi1iAOG9-SqCBTSHtjH8CrsIsXnzQ4c4Unj_a_qZ4Q7VXK3H0XtZCIaIJTTfWbemSWo0rQmiB_pskqUBdJ8RuLY3BrwiHbrLnoVeJENGPei06gk=w1920-h1080\"></object>\n" +
+        "  <object data=\"https://lh3.googleusercontent.com/vjODOWJTqlRe3jiZ7M4qPeo-zM6ydcfOTnwDvtZRpuO2Gw5Z4NTmJWQrxemux75dFPZ1Iq5atD62H6cRbh5jW5m6_oo-fD-JQIpG6CBVaruByHuq6cCz-6hr4DrULCkOeP9L9C1oH9Q=w1920-h1080\"></object>\n" +
+        "</div>\n");
+    $("div.overlay-wrapper").show();
   });
 
   $("img.close").on("click", function () {
-    $("div.pa-gallery-player-widget").hide();
-    $("img.close").hide();
+    $("div.pa-gallery-player-widget").remove();
+    $("div.overlay-wrapper").hide();
   });
 });
